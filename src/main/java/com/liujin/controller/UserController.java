605e6e1b4a63605a0e4ef01299f3e758e7816538
liujin <1532551496@qq.com>
2023-04-07 21:52:20 +0800

    Swagger项目

--- src/main/java/com/liujin/controller/UserController.java
+++ src/main/java/com/liujin/controller/UserController.java
@@ -3,6 +3,7 @@ package com.liujin.controller;
 import com.liujin.pojo.User;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,13 @@ public class UserController {
     //@ApiOperation方法的注解
     @ApiOperation("添加user")
     //@ApiParam是参数的注解
-    public String addUser(@ApiParam("用户信息") User user){
+    public String addUser(@ApiParam("用户") User user) {
         return "测试";
     }
+
+    @GetMapping
+    public User selectUser() {
+        return new User();
+    }
 }
+
